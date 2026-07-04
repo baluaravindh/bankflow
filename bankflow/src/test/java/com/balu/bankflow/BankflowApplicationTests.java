50567e8f9e956b7ce20e1490e8a9be2a15fd2819
dev global <balu.gounipuram@gmail.com>
2026-07-04 23:57:41 +0530

    BF-000 Initial project setup

---
+++ bankflow/src/test/java/com/balu/bankflow/BankflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.balu.bankflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
